7083bced8ea2e265225ff3c8290103f41dd2845d
Alan Weingartner <alan.weingartner@gmail.com>
2018-11-20 05:57:42 -0200

    fixed issue with JSON visibilty

--- models/user.go
+++ models/user.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-	ID         int64     `gorm:"AUTO_INCREMENT;PRIMARY_KEY"`
-	Username   string    `gorm:"type:varchar(20);unique;not null"`
-	PubKey     string    `gorm:"type:text;not null"`
-	RecoverKey string    `gorm:"type:text"`
-	FcmToken   string    `gorm:"type:varchar(255)"`
-	CreatedAt  time.Time `gorm:"type:timestamp"`
-	UpdatedAt  time.Time `gorm:"type:timestamp"`
-	DeletedAt  time.Time `gorm:"type:timestamp;default:null"`
+	ID         int64     `gorm:"AUTO_INCREMENT;PRIMARY_KEY" json:",omitempty"`
+	Username   string    `gorm:"type:varchar(20);unique;not null" json:",omitempty"`
+	PubKey     string    `gorm:"type:text;not null" json:",omitempty"`
+	RecoverKey string    `gorm:"type:text" json:"-"`
+	FcmToken   string    `gorm:"type:varchar(255)" json:"-"`
+	CreatedAt  time.Time `gorm:"type:timestamp" json:",omitempty"`
+	UpdatedAt  time.Time `gorm:"type:timestamp" json:"-"`
+	DeletedAt  time.Time `gorm:"type:timestamp;default:null" json:"-"`
 }
