9f406541cf8f5017ac095c7ffe028ed8fac41e12
Alan Weingartner <alan.weingartner@gmail.com>
2018-11-25 12:52:18 -0200

    fixed timing verification

--- protoutil/messageproto.go
+++ protoutil/messageproto.go
@@ -9,11 +9,11 @@ import (
 
 // MessageModelToProto maps one ore more Message model into an array of
 // Message that was created to communicate messages between users
-func MessageModelToProto(ms ...models.Message) []*protos.Message {
+func MessageModelToProto(us string, ms ...models.Message) []*protos.Message {
 	var ps []*protos.Message
 
 	for _, m := range ms {
-		d := m.CreatedAt.Add(time.Duration(5) * time.Minute).Before(time.Now())
+		d := us == m.Sender.Username && m.CreatedAt.Add(time.Duration(5)*time.Minute).After(time.Now())
 		p := &protos.Message{
 			From:      m.Sender.Username,
 			To:        m.Receiver.Username,
