7578d47d80a900d2e08ee42c5bcc2eb6649abd07
Alan Weingartner <alan.weingartner@gmail.com>
2018-11-25 12:51:24 -0200

    added nonce and message type to the model

--- models/messagecontent.go
+++ models/messagecontent.go
@@ -3,6 +3,8 @@ package models
 type MessageContent struct {
 	MessageID int64  `gorm:"PRIMARY_KEY;REFERENCES apate.user(id)"`
 	Body      string `gorm:"type:text;not null"`
+	Nonce     string `gorm:"type:text;not null"`
+	Type      int32  `gorm:"type:text;not null"`
 
 	Message *Message `gorm:"foreignkey:MessageID;association_foreignkey:ID"`
 }
